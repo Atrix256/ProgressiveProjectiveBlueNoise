2e7dc2d2338cd6760c6025e2dd6fdd29dd4e591f
Alan Wolfe <alan.wolfe@gmail.com>
2018-10-28 21:03:36 -0700

    checkpoint working through a generic N dimensional subspace grid proximity acceleration structure.

--- main.cpp
+++ main.cpp
@@ -291,6 +291,258 @@ void GoodCandidateAlgorithm(std::vector< std::array<float, DIMENSION>>& results,
     }
 }
 
+template <size_t DIMENSION, size_t PARTITIONS>
+struct GoodCandidateSubspace
+{
+    typedef std::array<float, DIMENSION> T;
+
+    void Init(size_t scoreIndex)
+    {
+        size_t partitionCount = 1;
+        int multiplier = 1;
+        for (size_t dimensionIndex = 0; dimensionIndex < DIMENSION; ++dimensionIndex)
+        {
+            axisMask[dimensionIndex] = (scoreIndex & (size_t(1) << dimensionIndex)) ? false : true;
+
+            if (!axisMask[dimensionIndex])
+            {
+                axisPartitionOffset[dimensionIndex] = 0;
+                continue;
+            }
+
+            axisPartitionOffset[dimensionIndex] = multiplier;
+            partitionCount *= PARTITIONS;
+            multiplier *= PARTITIONS;
+        }
+
+        partitionedPoints.resize(partitionCount);
+        partitionChecked.resize(partitionCount);
+    }
+
+    int PartitionForPoint(const T&point) const
+    {
+        int subspacePartition = 0;
+        int multiplier = 1;
+        for (size_t dimensionIndex = 0; dimensionIndex < DIMENSION; ++dimensionIndex)
+        {
+            if (!axisMask[dimensionIndex])
+            {
+                continue;
+            }
+
+            int axisPartition = int(0.5f + point[dimensionIndex] * float(PARTITIONS - 1));
+            subspacePartition += axisPartition * multiplier;
+            multiplier *= PARTITIONS;
+        }
+
+        // TODO: use axisPartitionOffset[] instead of duplicating multiplier logic!
+
+        return subspacePartition;
+    }
+
+    float SquaredDistanceToClosestPointRecursive(const T& point, int subspacePartition, int radius, size_t dimensionIndex)
+    {
+        // if we have run out of dimensions, it's time to search a partition
+        if (dimensionIndex == DIMENSION)
+        {
+            // if we've already checked this partition, nothing to do.
+            // otherwise, mark is as checked so it isn't checked again.
+            if (partitionChecked[subspacePartition])
+                return FLT_MAX;
+            partitionChecked[subspacePartition] = true;
+
+            // TODO: maybe null out the axes that don't matter, on the inserted points.
+            // TODO: maybe also mull out the axes that don't matter on the search point higher up the chain, to make it cheaper.
+            float minDistSq = FLT_MAX;
+            for (auto& p : partitionedPoints[subspacePartition])
+            {
+                float distSq = 0.0f;
+                for (size_t i = 0; i < DIMENSION; ++i)
+                {
+                    if (!axisMask[i])
+                        continue;
+
+                    float diff = fabsf(p[i] - point[i]);
+                    if (diff > 0.5f)
+                        diff = 1.0f - diff;
+                    distSq += diff * diff;
+                }
+                minDistSq = std::min(minDistSq, distSq);
+            }
+            return minDistSq;
+        }
+
+        // if this axis doesn't participate, ignore it!
+        if (!axisMask[dimensionIndex])
+            return SquaredDistanceToClosestPointRecursive(point, subspacePartition, radius, dimensionIndex + 1);
+
+        // loop through this axis radius and return the smallest value we've found
+        float ret = FLT_MAX;
+        for (int axisOffset = -radius; axisOffset <= radius; ++axisOffset)
+        {
+            // TODO: handle going out of bounds with partitions!!
+
+            int searchSubspacePartition = subspacePartition + axisOffset * axisPartitionOffset[dimensionIndex];
+            ret = std::min(ret, SquaredDistanceToClosestPointRecursive(point, searchSubspacePartition, radius, dimensionIndex + 1));
+        }
+        return ret;
+    }
+
+    float SquaredDistanceToClosestPoint(const T& point)
+    {
+        // mark all partitions as having not been checked yet
+        std::fill(partitionChecked.begin(), partitionChecked.end(), false);
+
+        // get the partition this point is in
+        int subspacePartition = PartitionForPoint(point);
+
+        // Loop through increasingly larger rectangular rings until we find a ring that has at least one point.
+        // Return the distance to the closest point in that ring.
+        int maxRadius = int(PARTITIONS / 2);
+        for (int radius = 0; radius <= maxRadius; ++radius)
+        {
+            float distance = SquaredDistanceToClosestPointRecursive(point, subspacePartition, radius, 0);
+            if (distance < FLT_MAX)
+                return distance;
+        }
+
+        return FLT_MAX;
+    }
+    
+    void Insert(const T& point)
+    {
+        int subspacePartition = PartitionForPoint(point);
+        partitionedPoints[subspacePartition].push_back(point);
+    }
+
+    std::vector<std::vector<T>> partitionedPoints;
+    std::vector<bool> partitionChecked;
+    std::array<bool, DIMENSION> axisMask;
+    std::array<int, DIMENSION> axisPartitionOffset;
+};
+
+template <size_t DIMENSION, size_t PARTITIONS>
+void GoodCandidateAlgorithmAccell(std::vector< std::array<float, DIMENSION>>& results, size_t desiredItemCount, int candidateMultiplier, bool reportProgress)
+{
+    typedef std::array<float, DIMENSION> T;
+
+    static std::random_device rd;
+    static std::mt19937 rng(rd());
+    static std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+
+    // map candidate index to score
+    struct CandidateScore
+    {
+        size_t index;
+        float score;
+    };
+    typedef std::vector<CandidateScore> CandidateScores;
+    static const size_t c_numScores = (1 << DIMENSION) - 1;  // 2^(dimension)-1
+
+    // setup the acceleration structures
+    std::array<GoodCandidateSubspace<DIMENSION, PARTITIONS>, c_numScores> subspaces;
+    for (size_t scoreIndex = 0; scoreIndex < c_numScores; ++scoreIndex)
+        subspaces[scoreIndex].Init(scoreIndex);
+
+    // TODO: when adding new points, also add them to the acceleration structure
+    // TODO: when comparing points, use the acceleration structure to find the nearest cells.
+
+    // make space for the results
+    results.resize(desiredItemCount);
+
+    int lastPercent = -1;
+
+    // for each item we need to fill in
+    for (int itemIndex = 0; itemIndex < desiredItemCount; ++itemIndex)
+    {
+        // calculate how many candidates we want to generate for this item
+        int candidateCount = itemIndex * candidateMultiplier + 1;
+
+        // generate the candidates
+        std::vector<T> candidates;
+        candidates.resize(candidateCount);
+        for (T& candidate : candidates)
+        {
+            for (size_t i = 0; i < DIMENSION; ++i)
+                candidate[i] = dist(rng);
+        }
+
+        // initialize the overall scores
+        CandidateScores overallScores;
+        overallScores.resize(candidateCount);
+        for (int candidateIndex = 0; candidateIndex < candidateCount; ++candidateIndex)
+        {
+            overallScores[candidateIndex].index = candidateIndex;
+            overallScores[candidateIndex].score = 0.0f;
+        }
+
+        // allocate space for the individual scores
+        CandidateScores scores;
+        scores.resize(candidateCount);
+
+        // score the candidates by each measure of scoring
+        for (size_t scoreIndex = 0; scoreIndex < c_numScores; ++scoreIndex)
+        {
+            // get the subspace we are working with
+            auto& subspace = subspaces[scoreIndex];
+
+            // for each candidate in this score index...
+            for (size_t candidateIndex = 0; candidateIndex < candidateCount; ++candidateIndex)
+            {
+                // calculate the score of the candidate.
+                // the score is the minimum distance to any other points
+                scores[candidateIndex].index = candidateIndex;
+                scores[candidateIndex].score = subspace.SquaredDistanceToClosestPoint(candidates[candidateIndex]);
+            }
+
+            // sort the scores from high to low
+            std::sort(
+                scores.begin(),
+                scores.end(),
+                [] (const CandidateScore& A, const CandidateScore& B)
+                {
+                    return A.score > B.score;
+                }
+            );
+
+            // add the rank of this score a score for each candidate
+            for (size_t candidateIndex = 0; candidateIndex < candidateCount; ++candidateIndex)
+                overallScores[scores[candidateIndex].index].score += float(candidateIndex);
+        }
+
+        // sort the overall scores from low to high
+        std::sort(
+            overallScores.begin(),
+            overallScores.end(),
+            [] (const CandidateScore& A, const CandidateScore& B)
+            {
+                return A.score < B.score;
+            }
+        );
+
+        // keep the point that had the lowest summed rank
+        results[itemIndex] = candidates[overallScores[0].index];
+
+        // insert this point into the acceleration structures
+        for (size_t scoreIndex = 0; scoreIndex < c_numScores; ++scoreIndex)
+            subspaces[scoreIndex].Insert(results[itemIndex]);
+
+        // report our percentage done if we should
+        if (reportProgress)
+        {
+            int percent = int(100.0f * float(itemIndex) / float(desiredItemCount));
+            if (lastPercent != percent)
+            {
+                lastPercent = percent;
+                printf("\rMaking Points: %i%%", lastPercent);
+            }
+        }
+    }
+
+    if (reportProgress)
+        printf("\rMaking Points: 100%%\n");
+}
+
 void GeneratePoints_WhiteNoise(std::vector<Vec2>& points, size_t numPoints)
 {
     std::uniform_real_distribution<float> dist(0.0f, 1.0f);
@@ -323,7 +575,7 @@ void GeneratePoints_BlueNoise(std::vector<Vec2>& points, size_t numPoints)
 
 void GeneratePoints_ProjectiveBlueNoise(std::vector<Vec2>& points, size_t numPoints)
 {
-    GoodCandidateAlgorithm(points, numPoints, 100, true);
+    GoodCandidateAlgorithmAccell<2,10>(points, numPoints, 100, true);
 }
 
 template <typename T>
@@ -633,11 +885,13 @@ int main(int argc, char **argv)
 }
 
 /*
-
 TODO:
+
+* in good candidate algorithm, rename scores terminology to subspaces
 
 * test projective blue noise with lower sample count but higher multiplier
  * find a good value for projective blue noise, even if it's restrictively high?
+ * should be better after acceleration structures are added
 
 * acceleration structure for blue noise and projective blue noise
 
