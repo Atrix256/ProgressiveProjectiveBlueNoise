642846b12fff08fb90fd1defe33a411bb4cb86ec
Alan Wolfe <alan.wolfe@gmail.com>
2018-11-07 19:46:38 -0800

    quick update

--- pathtrace.cpp
+++ pathtrace.cpp
@@ -39,6 +39,64 @@ static const Sphere c_Lights[] =
     {{0.0f, 1.0f, 0.0f}, 0.1f}
 };
 
+inline Vec3 Normalize(const Vec3& v)
+{
+    float len = sqrtf(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
+    Vec3 ret;
+    ret[0] = v[0] / len;
+    ret[1] = v[1] / len;
+    ret[2] = v[2] / len;
+    return ret;
+}
+
+inline Vec3 Cross(const Vec3& a, const Vec3& b)
+{
+    return
+    {
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]
+    };
+}
+
+inline Vec3 operator * (const Vec3& v, float f)
+{
+    return
+    {
+        v[0] * f,
+        v[1] * f,
+        v[2] * f,
+    };
+}
+
+inline Vec3 operator += (Vec3& a, const Vec3& b)
+{
+    a[0] += b[0];
+    a[1] += b[1];
+    a[2] += b[2];
+    return a;
+}
+
+inline Vec3 operator + (const Vec3& a, const Vec3& b)
+{
+    return
+    {
+        a[0] + b[0],
+        a[1] + b[1],
+        a[2] + b[2],
+    };
+}
+
+inline Vec3 operator - (const Vec3& a, const Vec3& b)
+{
+    return
+    {
+        a[0] - b[0],
+        a[1] - b[1],
+        a[2] - b[2],
+    };
+}
+
 void SamplePixel(float* pixel, const Vec3& rayPos, const Vec3& rayDir, size_t startSampleCount, size_t endSampleCount)
 {
     // TODO: find the initial hit out of the loop since it will be the same each time. Note that we are not doing subpixel jitter, so there will be geo aliasing.
@@ -68,8 +126,8 @@ void PathtraceTest(ImageFloat& image, size_t startSampleCount, size_t endSampleC
     const float c_cameraHorizFOV = c_ptCameraVerticalFOV * c_aspectRatio;
     const float c_windowTop = tan(c_ptCameraVerticalFOV / 2.0f) * c_ptNearPlaneDistance;
     const float c_windowRight = tan(c_cameraHorizFOV / 2.0f) * c_ptNearPlaneDistance;
-    //const Vec3 c_cameraRight = Cross({ 0.0f, 1.0f, 0.0f }, c_ptCameraFwd);
-    //const Vec3 c_cameraUp = Cross(c_ptCameraFwd, c_cameraRight);
+    const Vec3 c_cameraRight = Cross({ 0.0f, 1.0f, 0.0f }, c_ptCameraFwd);
+    const Vec3 c_cameraUp = Cross(c_ptCameraFwd, c_cameraRight);
 
     float* pixel = image.m_pixels.data();
 
@@ -85,15 +143,12 @@ void PathtraceTest(ImageFloat& image, size_t startSampleCount, size_t endSampleC
             v = v * 2.0f - 1.0f;
 
             // find where the ray hits the near plane, and normalize that vector to get the ray direction.
-            /*
             Vec3 rayPos = c_ptCameraPos + c_ptCameraFwd * c_ptNearPlaneDistance;
             rayPos += c_cameraRight * c_windowRight * u;
             rayPos += c_cameraUp * c_windowTop * v;
             Vec3 rayDir = Normalize(rayPos - c_ptCameraPos);
-            */
 
-
-            //SamplePixel(pixel, rayPos, rayDir, startSampleCount, endSampleCount);
+            SamplePixel(pixel, rayPos, rayDir, startSampleCount, endSampleCount);
             pixel += 4;
         }
     }
